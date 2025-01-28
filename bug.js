694b64c2fff20fb1aff88e1b33ed666e0a258b94
BugHunter-X <askzenai@gmail.com>
2025-01-28 21:00:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// Example component
+<div class="bg-gray-100 p-4">
+  <h2 class="text-xl font-bold mb-2">Uncommon TailwindCSS Bug</h2>
+  <p>This is where I describe my bug.</p>
+</div>
+```
